8935de2c6f4ceb448a6d247d2290c48d19fa72d1
danila <da_buddha@bk.ru>
2023-01-20 01:04:37 +0300

    prepare project for firebase deploy

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
 import TheChat from "./components/TheChat.vue";
 import { ref } from "vue";
-const ad = ref(10);
-const n = ref(false);
+const adhesion = ref(10);
+const nailed = ref(false);
 // setTimeout(() => (ad.value = 100), 5000);
 // setInterval(() => (n.value = !n.value), 4000);
 </script>
 
 <template>
-  <TheChat :nailed="n" :key="n" :adhesion="ad" />
+  <div>
+    Включить прилипание? <input v-model="nailed" max="100" type="checkbox" />
+  </div>
+  <div>
+    Сколько пикселей адгезия?
+    <input v-model="adhesion" type="number" />
+  </div>
+  <TheChat :nailed="nailed" :key="nailed" :adhesion="adhesion" />
 </template>
 
-<style scoped></style>
+<style scoped>
+div {
+  color: #fff;
+  font-size: 1.5rem;
+}
+</style>
